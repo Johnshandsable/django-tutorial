37af8ca8f6809fc67521d2a42a9bfbae2a835b9a
Johnshandsable <Johnshandsable@outlook.com>
2021-05-14 10:39:30 -0500

    created a serializer for room

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -13,6 +13,7 @@ def generate_unique_code():
         code = ''.join(random.choices(string.ascii_uppercase, k=length))
         if Room.objects.filter(code=code).count() == 0: 
             break
+    return code
 
 # Create your models here.
 class Room(models.Model): 
