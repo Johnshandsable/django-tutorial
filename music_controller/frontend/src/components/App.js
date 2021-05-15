44e03ee57714d32e319a8cb5bfbe1436ea55cd82
Johnshandsable <Johnshandsable@outlook.com>
2021-05-15 16:53:14 -0500

    added a urls for frontend

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom'; 
 
+import Home from './Home';
+
 export default class App extends Component {
     constructor(props) {
         super(props);
@@ -8,10 +10,10 @@ export default class App extends Component {
 
     render() {
         return (
-            <h1>Testing React Application</h1>
+            <Home/>
         )
     }
 }
 
 const appDiv = document.getElementById("app"); 
-render(<App/>, appDiv);
+render(<App />, appDiv);
