07732b6efa043cc1744a00c4cf404b75de7f11a8
Johnshandsable <Johnshandsable@outlook.com>
2021-05-17 13:46:39 -0500

    updated models.py for new object

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import RoomSerializer
+from rest_framework import generics, status
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
+from rest_framework.view import APIView 
+from rest_framework.response import Response
+
 
 # Create your views here.
-
-
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView): 
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None): 
+        if not self.request.session.exists(self.request.session.session_key): 
+            self.request.session.creatE()
+        
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(): 
+            guest_can_pause = serializer.data.guest_can_pause 
+            votes_to_skip = serializer.data.votes_to_skip
+            host = self.request.session.session_key
